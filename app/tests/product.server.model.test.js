cf2d96b993a3e9a5fd95e03653c69dca47af4b19
Kevin Lanzon <kevinlanzon@gmail.com>
2016-03-02 17:33:20 +0000

    fixes indentation

--- app/tests/product.server.model.test.js
+++ app/tests/product.server.model.test.js
@@ -4,9 +4,9 @@
  * Module dependencies.
  */
 var should = require('should'),
-	mongoose = require('mongoose'),
-	User = mongoose.model('User'),
-	Product = mongoose.model('Product');
+  mongoose = require('mongoose'),
+  User = mongoose.model('User'),
+  Product = mongoose.model('Product');
 
 /**
  * Globals
@@ -17,22 +17,22 @@ var user, product;
  * Unit tests
  */
 describe('Product API', function() {
-	beforeEach(function(done) {
-		user = new User({
-			firstName: 'Full',
-			lastName: 'Name',
-			displayName: 'Full Name',
-			email: 'test@test.com',
-			username: 'username',
-			password: 'password',
-			provider: 'local'
-		});
-		user.save(done);
-	});
+  beforeEach(function(done) {
+    user = new User({
+      firstName: 'Full',
+      lastName: 'Name',
+      displayName: 'Full Name',
+      email: 'test@test.com',
+      username: 'username',
+      password: 'password',
+      provider: 'local'
+    });
+    user.save(done);
+  });
 
-	afterEach(function(done) {
-		Product.remove().exec();
-		User.remove().exec();
-		done();
-	});
+  afterEach(function(done) {
+    Product.remove().exec();
+    User.remove().exec();
+    done();
+  });
 });
