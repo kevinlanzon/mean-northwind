aa0baa792781a5cccba44a47f4834d764108287c
Kevin Lanzon <kevinlanzon@gmail.com>
2016-03-03 10:54:52 +0000

    adds authentication for products

--- app/routes/products.server.routes.js
+++ app/routes/products.server.routes.js
@@ -2,16 +2,17 @@
 
 module.exports = function(app) {
   // Require functions from products controller
-  var products = require('../../app/controllers/products.server.controller');
+  var products = require('../../app/controllers/products.server.controller'),
+      users = require('../../app/controllers/users.server.controller');
 
   app.route('/products')
     .get(products.list)
-    .post(products.create);
+    .post(users.requiresLogin, products.create);
 
   app.route('/products/:productId')
     .get(products.read)
-    .put(products.update)
-    .delete(products.delete);
+    .put(users.requiresLogin, products.update)
+    .delete(users.requiresLogin, products.delete);
 
   // Where the productId is present in the URL
   // the logic to 'get by id' is handled by this single function
